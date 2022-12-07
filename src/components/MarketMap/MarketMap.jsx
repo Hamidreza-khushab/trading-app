cf0f0cc33bc272819fee9afdd1ece4a86fb1c8a3
Hamidreza Khushab <85607611+Hamidreza-khushab@users.noreply.github.com>
2022-12-07 13:04:13 +0100

    add compelet market map chart

--- src/components/MarketMap/MarketMap.jsx
+++ src/components/MarketMap/MarketMap.jsx
@@ -1,19 +1,23 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { CryptoContext } from '../../CryptoContext';
-
+import { Treemap, ResponsiveContainer } from 'recharts';
 import Modal from 'react-bootstrap/Modal';
 
 const MarketMap = ({ showMarket, hideModal }) => {
     const { price, setPrice } = useContext(CryptoContext)
-    const [totalCap , setTotalCap] = useState(0)
+    const [data, setData] = useState([]);
     useEffect(() => {
-        let sum = 0;
+        let sum = 0 
+        data.splice(0)
         for (let i = 0; i < price.length; i++) {
-            sum = parseFloat(price[i].market_cap) + sum;
+            if (parseFloat(price[i].market_cap) > 1800000000.00) {
+                sum = sum + 1 
+                setData(data => [{ children: [{ name: price[i].symbol, size: parseFloat(price[i].market_cap) }] }, ...data])
+            }
+            console.log(sum);
         }
-        setTotalCap(sum)
-        console.log('totalCap',totalCap);
     }, [])
+    console.log('data000', data);
     return <>
         <Modal
             show={showMarket} fullscreen={true} onHide={() => hideModal()}>
@@ -22,7 +26,9 @@ const MarketMap = ({ showMarket, hideModal }) => {
             </Modal.Header>
             <Modal.Body>
                 {/* *********************************************************** */}
-
+                <ResponsiveContainer width="100%" height="100%">
+                    <Treemap width={400} height={200} data={data} dataKey="size" ratio={4 / 3} stroke="#fff" fill="#8884d8" />
+                </ResponsiveContainer>
                 {/* ********************************************************** */}
             </Modal.Body>
         </Modal>
